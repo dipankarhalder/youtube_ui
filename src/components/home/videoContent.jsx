742cbf08547cec196895b97206ef806c356e2650
dipankarhalder <webmail.dip@gmail.com>
2023-09-25 03:17:30 +0530

    updated code

---
+++ src/components/home/videoContent.jsx
@@ -0,0 +1,13 @@
+export const VideoContent = () => {
+  return (
+    <ul>
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
+        <li key={item}>
+          <span></span>
+          <h4>ksdjjhfjhkdsfg</h4>
+          <p>sdkghj</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,3 +1,10 @@
+import './pages.css';
+import { VideoContent } from '../components/home/videoContent';
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="app_cover_video_content">
+      <VideoContent />
+    </div>
+  );
 };
